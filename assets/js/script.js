5a53a3cf1b69d9d6c6be1d034a9321fc1267e7d0
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-03-16 20:46:26 +1100

    event-listener-fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ $('#currentDay').text(today.format('dddd, MMMM D'));
 
 $(document).ready(function () {
     // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
+    $('.saveBtn').on('click', function () {
+        console.log('bam')
         // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -46,7 +47,7 @@ $(document).ready(function () {
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
     $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour11 .description").val(localStorage.getItem("hour1"));
     $("#hour2 .description").val(localStorage.getItem("hour2"));
     $("#hour3 .description").val(localStorage.getItem("hour3"));
     $("#hour4 .description").val(localStorage.getItem("hour4"));
